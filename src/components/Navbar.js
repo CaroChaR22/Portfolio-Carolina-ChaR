ce475783063d1bcf3cacb1200bc43c545b771575
CaroChaR <you@example.com>
2022-10-12 00:09:16 -0500

    first commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import * as S from './Navbar.styles';
+
+
+const Navbar = () => {
+  return (
+    <S.ContainerNavbar> esto es el componente del navbar</S.ContainerNavbar>
+
+  )
+
+}
+
+export default Navbar;
+
+
+// rafce == react functional component
+// se acreado una variable en el archivo de estilos del navbar
+//  se importan todas laviables del archivo de estilos como se indica en la linea 2
+    // se crea un contenedor donde se hará toda la funcionalidad 
---
+++ src/components/Navbar.styles.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const ContainerNavbar = styled.div`
+height: 100px;
+background-color: pink;
+`;
+
+
+
+
+// se exportan los estilos para que sean importados en el archivo Navbar.js  ()
