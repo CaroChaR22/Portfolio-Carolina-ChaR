217025c940dc2602d68c580f88e617097741a029
CaroChaR <you@example.com>
2022-10-12 19:05:00 -0500

    feat: add burguer menu mobile and desktop

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,14 +1,40 @@
-import React from 'react'
-import * as S from './Navbar.styles';
-
+import { useState } from "react";
+import React from "react";
+import * as S from "./Navbar.styles";
 
 const Navbar = () => {
+  const [showMenuBurguer, setShowMenuBurguer] = useState(false);
+  console.log(showMenuBurguer);
+
+
   return (
-    <S.ContainerNavbar> esto es el componente del navbar</S.ContainerNavbar>
+    <S.ContainerNavbar>
+      <S.ContainerLogo>
+        <S.Logo src="" alt="" />
+        <S.Titles>
+          <h1>Carolina ChaR</h1>
+          <h2>Desarrolladora Full Stack</h2>
+        </S.Titles>
+      </S.ContainerLogo>
 
-  )
+      <S.ContainerBurguer onClick={() => setShowMenuBurguer(!showMenuBurguer)}>
+        <S.MenuBurguer />
+        <S.MenuBurguer />
+        <S.MenuBurguer />
+      </S.ContainerBurguer>
 
-}
+      <S.ContainerItems showMenuBurguer={ showMenuBurguer}>
+        <p>About me</p>
+        <p>Projects</p>
+
+        <S.ContainerButton>
+          <div>📞 Contact me</div>
+        </S.ContainerButton>
+        
+      </S.ContainerItems>
+    </S.ContainerNavbar>
+  );
+};
 
 export default Navbar;
 
@@ -16,4 +42,4 @@ export default Navbar;
 // rafce == react functional component
 // se acreado una variable en el archivo de estilos del navbar
 //  se importan todas laviables del archivo de estilos como se indica en la linea 2
-    // se crea un contenedor donde se hará toda la funcionalidad 
+// se crea un contenedor donde se hará toda la funcionalidad
--- src/components/Navbar.styles.js
+++ src/components/Navbar.styles.js
@@ -1,8 +1,115 @@
 import styled from "styled-components";
+// import { colors } from "../../styles/colors";
+import { colors } from "../styles/colors";
 
 export const ContainerNavbar = styled.div`
-height: 100px;
-background-color: pink;
+  max-width: 100vw;
+  height: 100px;
+  box-shadow: ${colors.BOX_SHADOW};
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 0 20px;
+  background: ${colors.BACKGROUND_COLOR};
+`;
+
+export const ContainerLogo = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+export const Titles = styled.title`
+  display: inline-block;
+  //   align-items: center;
+
+  h1,
+  h2 {
+    font-weight: 700;
+    color: ${colors.TEXT_BLACK};
+    margin: 0;
+  }
+
+  h1 {
+    font-size: 2rem;
+  }
+
+  h2 {
+    background: linear-gradient(135deg, #cb5eee, 20%, #4be1ec 100%);
+    background-clip: text;
+    -webkit-background-clip: text;
+    -webkit-text-fill-color: transparent;
+  }
+`;
+
+export const Logo = styled.img`
+  width: 90px;
+  padding-right: 20px;
+`;
+
+export const ContainerItems = styled.div`
+  display: flex;
+  align-items: center;
+
+  p {
+    cursor: pointer;
+    padding: 0 13px;
+    text-decoration: none;
+    color: ${colors.TEXT_BLACK};
+    font-weght: 500;
+  }
+
+  @media (max-width: 768px) {
+    display: ${(props) => (props.showMenuBurguer ? "inline-grid" : "none")};
+    position: absolute;
+    top: 100px;
+    background: ${colors.TEXT_WHITE};
+    box-shadow: ${colors.BOX_SHADOW_BLUE};
+    padding: 10px 0 20px 0;
+    width: 100%;
+    left: 0;
+
+    a {
+      padding: 10px 20px;
+    }
+  }
+`;
+
+
+export const ContainerButton = styled.button`
+  display: flex;
+  align-items: center;
+  color: ${colors.TEXT_BLACK};
+  border-radius: 999px;
+  margin: 10px 0;
+  cursor: pointer;
+  border: none;
+
+  &:hover {
+    background-color: LightGray;
+    padding: 5px 10px;
+    box-shadow: 0px 1px 1px 3px gray;
+    margin: 10px 0;
+  }
+
+  @media (max-width: 768px) {
+    width: 50%;
+    margin: 5px 10px 0 10px;
+  }
+`;
+
+export const  ContainerBurguer= styled.div`
+display: none;
+
+@media (max-width: 768px) {
+  display: block;
+}
+`;
+
+export const MenuBurguer = styled.div`
+width: 35px;
+height: 2px;
+background-color: ${colors.TEXT_BLACK};
+margin: 6px 0;
 `;
 
 
